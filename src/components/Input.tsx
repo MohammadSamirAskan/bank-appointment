c9bcc79a4a0c13a2d4cbfad3eac2405afb110140
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2023-03-19 20:45:34 +0300

    Create custom input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+type CustomProps = {
+	wrapperStyle?: React.CSSProperties;
+	wrapperClassName?: string;
+	replaceWrapperCls?: boolean;
+	replaceClass?: boolean;
+	isInvalid?: boolean;
+};
+
+type InputProps = CustomProps & React.ComponentProps<"input">;
+
+function Input({
+	wrapperClassName = "",
+	wrapperStyle = {},
+	replaceWrapperCls = false,
+	className = "",
+	replaceClass = false,
+	isInvalid = false,
+	style,
+	...props
+}: InputProps) {
+	return (
+		<div
+			className={
+				replaceWrapperCls
+					? wrapperClassName
+					: `w-full border-[1px] rounded-md ${
+							!isInvalid
+								? "border-appGray/50 hover:ring-[3px] hover:ring-grayborder-appGray/50 focus-within:border-appText"
+								: "border-appRed/50 hover:ring-[3px] hover:ring-appRed/50 focus-within:border-appRed"
+					  }  ${wrapperClassName}`
+			}
+			style={{ ...wrapperStyle }}
+		>
+			<input
+				className={
+					replaceClass
+						? className
+						: `w-full p-1 rounded-md border-none focus:outline-none ${className}`
+				}
+				{...props}
+				style={{ ...style }}
+			/>
+		</div>
+	);
+}
+
+export default Input;
