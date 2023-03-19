fec61a710a6e26c5cf05d52810567c210255c445
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2023-03-19 20:58:08 +0300

    Code the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import React from "react";
+import { Outlet, useLocation, useNavigate, useParams } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import SearchForm from "./components/SearchForm";
+
 export type AppointmentObj = {
   appointmentNo: number;
   firstName: string;
@@ -10,3 +16,43 @@ export type AppointmentObj = {
   date: number;
   hour: number;
 };
+
+function App() {
+  const { bookingNo } = useParams();
+  const [query, setQuery] = React.useState(bookingNo || "");
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    if (location.pathname === "/" && !bookingNo) {
+      setQuery("");
+    }
+  }, [location.pathname, bookingNo]);
+
+  return (
+    <div className="App">
+      <div className="print:hidden">
+        <Header />
+      </div>
+      <main className="px-5 flex flex-col w-full justify-center">
+        <div className="print:hidden">
+          <SearchForm
+            query={query}
+            setQuery={setQuery}
+            onSubmit={() => {
+              if (query === "") {
+                return navigate("/");
+              }
+              navigate("/search/" + query);
+            }}
+          />
+        </div>
+        <div className="max-w-xl self-center">
+          <Outlet />
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
